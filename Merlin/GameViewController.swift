40f6ecfeb06e56cb7d5a377cdcf8d3807fb0429a
Dean Kuhta <dean@iteration15.com>
2016-04-14 10:30:20 -0400

    udated readme

--- Merlin/GameViewController.swift
+++ Merlin/GameViewController.swift
@@ -17,7 +17,6 @@ class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupView()
         setupScene()
         setupCamera()
